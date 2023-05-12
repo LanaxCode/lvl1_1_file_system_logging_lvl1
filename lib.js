7499884505510f7ab03590aadd5f546e5d490775
LanaxCode <lana.supercode@gmail.com>
2023-05-12 15:29:59 +0200

    finfish lvl1

---
+++ lib.js
@@ -0,0 +1,27 @@
+import { existsSync } from 'node:fs';
+import { mkdir } from 'node:fs/promises';
+import { writeFile } from 'node:fs/promises';
+import { appendFile } from 'node:fs';
+
+export const logging = () => {
+
+    try {
+        if (existsSync("logs")) {
+            console.log("folder exists")
+        } else {
+            mkdir("logs")
+            writeFile("logs/log.txt", "hello")
+            console.log("folder created")
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
+
+    let mytext = "hallo Laura"
+    appendFile("logs/log.txt", + "\n" + mytext, function (err) {
+        if (err) throw (err);
+        console.log('saved')
+    })
+
+}
